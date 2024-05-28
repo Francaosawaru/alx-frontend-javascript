8886546b8b3af8dd1c412e8b36aaca257b3dd822
Francaosawaru <forierie@yahoo.com>
2024-05-28 03:10:49 +0000

    Task completed

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employees = '';
-  for (const item of reportWithIterator) {
-    employees += `${item} | `;
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
-  return employees.slice(0, employees.length-3);
+
+  return employees.join(' | ');
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments(...Kwargs) {
-  return Kwargs.length;
-
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
